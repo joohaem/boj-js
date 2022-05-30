04d0503c1179ef156a76a05acd5a54e0f4a342df
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-30 23:15:08 +0900

    Map & Set

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,18 +1,5 @@
-
-// let answer = -1;
-
-// const [...nums] = inputs.map((line) => {
-//   return line.split(" ").map(Number);
+ = inputs.map((input) => {
+//   return uniqueArr.indexOf(input);
 // });
 
-// nums.forEach((line) => {
-//   for (let num of line) {
-//     myQ.insert(num);
-
-//     if (myQ.size() > N) answer = myQ.pop();
-//   }
-// });
-
-// // -----------------------------------
-
-// console.log(answer);
+// console.log(answer.join(" "));
