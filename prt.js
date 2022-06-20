6910085d005fc23315085e618eb7d5aff07aa6fb
SNUPI <joohaem@daum.net>
2022-06-20 12:43:19 +0900

    [docs] 이전 값 기억하기 -> 스택/큐

--- prt.js
+++ prt.js
@@ -5,19 +5,6 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 // const n = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
 // const s = fs.readFileSync(filePath).toString().trim().replace(/\[]/g, "");
 
-const s = "baabaa";
-
 // -----------------------------------
-
-function solution(s) {
-  const stack = [];
-
-  for (let i = 0; i < s.length; i++) {
-    if (!stack.length || stack[stack.length - 1] !== s[i]) stack.push(s[i]);
-    else stack.pop();
-  }
-
-  return stack.length ? 0 : 1;
-}
 
 // -----------------------------------
